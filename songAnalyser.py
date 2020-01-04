1a0fce5fd52ea4b0640d973c9d46f7f2f4e59bb7
Ellie Harrison <ellie@vertigomoon.co.uk>
2020-01-04 02:31:41 +0000

    Change colour on pitch

--- songAnalyser.py
+++ songAnalyser.py
@@ -1,14 +1,12 @@
 import spotipy
 import time
 import json
-import cv2 as cv
-import numpy as np
+import threading
+
 class SongAnalyser:
     MS_PER_SECOND = 1000
     DEFAULT_TICKS_PER_SECOND = 60
     SIGNATURE_TYPES = {'bars', 'beats', 'tatums', 'sections', 'segments'}
-    
-
     
     def __init__(self, token: str, debug: bool = False):
         """
@@ -29,12 +27,27 @@ class SongAnalyser:
         self.currentSystemTime = time.time()
         self.isPlaying = False
         self.ticksSinceLastUpdate = 0
-        self.currentBeat = None
+        self.currentSignatures = {}
+        self.nextSignatures = {}
+        self.songProgress = 0
+        self.onEvent = None
+        self.onDraw = None
+        self.resetTime = False
+        self.t1 = threading.Thread(target=self.analyseSongLoop)
 
-    
+        for type in self.SIGNATURE_TYPES:
+            self.currentSignatures[type] = None
+            self.nextSignatures[type] = None
+
+    def onSignatureEvent(self, event):
+        self.onEvent = event
+
+    def onDraw(self, onDraw):
+        self.onDraw = onDraw
+
     def run(self):
         """Starts this Song Analyser, creating a loop to keep track of current song position"""
-        self.analyseSong()
+        self.t1.start()
         while True:
             self.mainLoop()
             
@@ -42,41 +55,50 @@ class SongAnalyser:
         if self.isPlaying:
             self.updateTime()
             self.updateSongState()
+            self.onDraw()
             time.sleep(1 / self.ticksPerSecond)
         #Currently this will update every 60 ticks, not strictly every second if there is any sort of delay between ticks.
-        if(self.ticksSinceLastUpdate % (self.ticksPerSecond * 4) == 0):
-            self.analyseSong()
         self.ticksSinceLastUpdate+=1
-        img = np.zeros((512,512,3), np.uint8)
-        cv.rectangle(img, (0,0), (100,100), (255,255,255))
-        cv.imshow('', img)
+        
 
     def updateTime(self):
         """Determines the current position in the currently playing song by taking the delta since the last recorded system time vs now"""
-        delta = time.time() - self.currentSystemTime
-        self.currentSystemTime += delta
-        self.songProgress += int(delta * self.MS_PER_SECOND)
+        if self.resetTime:
+                self.spotify.seek_track(0)
+                self.currentSystemTime = time.time()
+                self.songProgress = 0
+                self.resetTime = False
+        else:
+            delta = time.time() - self.currentSystemTime
+            self.currentSystemTime += delta
+            self.songProgress += delta * self.MS_PER_SECOND
         if self.debug:
             print(self.songProgress)
             
     def updateSongState(self):
         #Todo, binary search the array damnit
         songTimeSeconds = self.songProgress / 1000
+        if self.analysis == None:
+            return
         for type in self.SIGNATURE_TYPES:
             signatures = self.analysis[type]
             signature = 0
             while signature < len(signatures):
                 signatureNow = signatures[signature]
-                signatureNext = signatures[signature + 1]
+                if signature == len(signatures) - 1:
+                    signatureNext = signatures[signature]
+                else:
+                    signatureNext = signatures[signature + 1] 
                 if((signatureNow['start'] < songTimeSeconds) & (songTimeSeconds < signatureNext['start'])) :
-                    if not self.currentBeat or self.currentBeat['start'] != signatureNow['start']:
-                        self.currentBeat = signatureNow
+                    if self.currentSignatures[type] == None or (self.currentSignatures[type]['start'] != signatureNow ['start']):
+                        self.currentSignatures[type] = signatureNow
+                        self.nextSignatures[type] = signatureNext
                         self.onSegmentChange(type, signatureNow)
                     break
                 signature+=1
 
 
-    def analyseSong(self):
+    def analyseSongLoop(self):
         """
         Checks the current song ID vs the Spotify Connect reported current song ID.\n
         If a change has occured, grab the song analysis data for the song.\n
@@ -85,23 +107,25 @@ class SongAnalyser:
         TODO: This really needs to be done on it's own thread as the REST calls to the Spotify Web API can introduce delay we really don't want
         """
         #This needs to be done on a thread at a regular interval
-        currentTrack = self.spotify.currently_playing()
-        if currentTrack:
-            currentId = currentTrack['item']['id']
-            preCallTime = time.time()
-            currentlyplaying = self.spotify.currently_playing()
-            self.songProgress = currentlyplaying['progress_ms'] - 1000
-            self.isPlaying = currentlyplaying['is_playing'] 
-            if currentId != self.analysedSong:
-                if self.debug:
-                    print(currentTrack['item']['name'])
-                    print('Analysing')
-                self.analysedSong = currentId
-                self.analysis = self.spotify.audio_analysis(self.analysedSong)
-            
-            
+        while True:
+            currentTrack = self.spotify.currently_playing()
+            if currentTrack:
+                currentId = currentTrack['item']['id']
+                preCallTime = time.time()
+                currentlyplaying = self.spotify.currently_playing()
+                self.isPlaying = currentlyplaying['is_playing'] 
+
+                if currentId != self.analysedSong:
+                    if self.debug:
+                        print(currentTrack['item']['name'])
+                        print('Analysing')
+                    self.analysedSong = currentId
+                    self.resetTime = True
+                    self.analysis = self.spotify.audio_analysis(self.analysedSong)
+
+            time.sleep(.1)
+                
     def onSegmentChange(self, segmentType: str, segment):
-        if segmentType == 'beats':
-            print(segmentType)
-            print(segment['confidence'])
+        self.onEvent(self, segmentType)
+
         
--- spotifyVisualiser.py
+++ spotifyVisualiser.py
@@ -1,14 +1,64 @@
 import spotipy.util as util
 import sys
 from songAnalyser import SongAnalyser
+import cv2 as cv
+import numpy as np
 
 REQUIRED_PERMISSION_SCOPE = 'user-read-playback-state user-modify-playback-state'
+COLOUR_MAX = 255
+img = np.zeros((50,50,3), np.uint8)
+currentColour = 0
+targetColour = 0
 
 def getToken():
     username = sys.argv[1]
     token: str = util.prompt_for_user_token(username, REQUIRED_PERMISSION_SCOPE)
     return token
-    
+
+def onEvent(state, type):
+    if type == 'beats':
+        global currentColour
+        global targetColour
+
+        pitches = songState.currentSignatures['segments']['pitches']
+
+        r = int(COLOUR_MAX * np.mean(pitches[0:3]))
+        g = int(COLOUR_MAX * np.mean(pitches[4:7]))
+        b = int(COLOUR_MAX * np.mean(pitches[8:11]))
+
+
+        currentColour = r << 16 | g << 8 | b
+
+    return
+        
+def onDraw():
+    if songState.currentSignatures['beats'] == None:
+        return
+    segmentProgress = songState.songProgress / 1000 - songState.currentSignatures['beats']['start']
+    segmentEnd = songState.nextSignatures['beats']['start'] - songState.currentSignatures['beats']['start']
+
+    percent = 1 - (segmentProgress / segmentEnd)
+    global currentColour
+
+
+    r = ((currentColour & 0xFF0000) >> 16) * percent
+    g = ((currentColour & 0x00FF00) >> 8) * percent
+    b = (currentColour & 0x0000FF) * percent
+
+    print(r)
+    print(g)
+    print(b)
+
+    print('=========')
+
+
+    img[:,0:50] = (b,g,r)     
+
+    cv.imshow('Beat Visualiser', img)
+    cv.waitKey(1)
+
 if __name__ == "__main__":
     songState = SongAnalyser(getToken(), False)
+    songState.onSignatureEvent(onEvent)
+    songState.onDraw = onDraw
     songState.run()
